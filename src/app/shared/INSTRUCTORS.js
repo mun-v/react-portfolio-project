22b9199e4926f2bd666b7edf145dfc10d38abfe9
Victoria Muñoz <mun.v203@gmail.com>
2022-06-26 23:41:36 -0500

    set up commit

---
+++ src/app/shared/INSTRUCTORS.js
@@ -0,0 +1,62 @@
+import adpfounder from '..assets/img/adpfounder';
+import Patricia from '..assets/img/Patricia';
+import Elisa from '..assets/img/Elisa';
+import Di from '..assets/img/Di';
+import Christina from '..assets/img/Christina';
+import Angie from '..assets/img/Angie';
+
+export const INSTRUCTORS = [
+    {
+        id: 0,
+        name: 'Alice',
+        image: adpfounder,
+        featured: false,
+        description:
+            // 'Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.'
+    },
+    {
+        id: 1,
+        name: 'Patricia Baab ',
+        image: Patricia,
+        // classes:
+        featured: false,
+        description:
+            // 'Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.'
+    },
+    {
+        id: 2,
+        name: 'Elisa Montoya', 
+        image: Elisa,
+        // classes:
+        featured: false,
+        description:
+            // 'Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.'
+    },
+    {
+        id: 3,
+        name: 'Di Flower',
+        image: Di,
+        // classes: 
+        featured: true,
+        description:
+            // "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
+    {
+        id: 4,
+        name: 'Christina Byram',
+        image: Christina,
+        // classes: 
+        featured: true,
+        description:
+            // "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
+    {
+        id: 5,
+        name: 'Angie Ward',
+        image: Angie,
+        // classes: 
+        featured: true,
+        description:
+            // "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
+];
