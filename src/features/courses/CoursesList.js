ab5090c43917d5dba2018c1fb4d52b6b1df30d39
Victoria Muñoz <mun.v203@gmail.com>
2022-06-29 21:28:46 -0500

    coursesSlice

--- src/features/courses/CoursesList.js
+++ src/features/courses/CoursesList.js
@@ -1,11 +1,13 @@
-import { COURSES } from "../../app/shared/COURSES";
 import { Col, Row } from "reactstrap";
 import CourseCard from "./CourseCard";
+import { selectAllCourses } from './coursesSlice';
 
 const CoursesList = () => {
+    const courses = selectAllCourses();
+
   return (
     <Row>
-      {COURSES.map((course) => {
+      {courses.map((course) => {
         return (
           <Col md="5" className="m-4" key={course.id}>
             <CourseCard course={course} />
