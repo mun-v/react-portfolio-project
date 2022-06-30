66f9ce6e9a9d0932c6c9c45edbc195aacac4dfad
Victoria Muñoz <mun.v203@gmail.com>
2022-06-29 22:42:45 -0500

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import CourseDirectoryPage from './pages/CourseDirectoryPage';
+import CourseDirectoryPage from "./pages/CourseDirectoryPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <CourseDirectoryPage />
-        <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CourseDirectoryPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
