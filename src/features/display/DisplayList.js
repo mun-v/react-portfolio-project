17571fa6c55b1aa9cd4d566ddb6eb5c22a3d06c8
Victoria Muñoz <mun.v203@gmail.com>
2022-07-01 13:00:45 -0500

    AnimatedDisplayCard

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,23 +1,29 @@
-import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
-import { selectFeaturedCourse } from '../courses/coursesSlice';
-import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
-import { selectFeaturedInstructor } from '../instructors/instructorsSlice';
+import { Col, Row } from "reactstrap";
+import { selectFeaturedCourse } from "../courses/coursesSlice";
+import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
+import { selectFeaturedInstructor } from "../instructors/instructorsSlice";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 
 const DisplayList = () => {
-    const items = [selectFeaturedCourse(), selectFeaturedPromotion(), selectFeaturedInstructor()];
+  const items = [
+    selectFeaturedCourse(),
+    selectFeaturedPromotion(),
+    selectFeaturedInstructor(),
+  ];
 
-    return (
-        <Row>
-            {items.map((item, idx) => {
-                return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
-                );
-            })}
-        </Row>
-    );
+  return (
+    <Row>
+      {items.map((item, idx) => {
+        return (
+          item && (
+            <Col md className="m-1" key={idx}>
+              <AnimatedDisplayCard item={item} />
+            </Col>
+          )
+        );
+      })}
+    </Row>
+  );
 };
 
 export default DisplayList;
