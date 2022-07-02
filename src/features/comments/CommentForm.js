65dd09e36d169a2bf9ebf132b69fad48a39f5502
Victoria Muñoz <mun.v203@gmail.com>
2022-07-01 19:04:49 -0500

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -7,11 +7,15 @@ import {
   FormGroup,
   Label,
 } from "reactstrap";
+import { useDispatch } from 'react-redux';
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { validateCommentForm } from "../../utils/validateCommentForm";
+import { addComment } from './commentsSlice';
 
 const CommentForm = ({ courseId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (values) => {
     const comment = {
@@ -19,8 +23,10 @@ const CommentForm = ({ courseId }) => {
       rating: values.rating,
       author: values.author,
       text: values.commentText,
+      date: new Date(Date.now()).toISOString()
     };
     console.log(comment);
+    dispatch(addComment(comment));
     setModalOpen(false);
   };
 
