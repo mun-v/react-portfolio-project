4a00bf5444d4ec36cf756493061dbed19b162483
Victoria Muñoz <mun.v203@gmail.com>
2022-07-02 21:14:17 -0500

    Week 4 Workshop

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,15 @@ import { coursesReducer } from '../features/courses/coursesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { instructorsReducer } from '../features/instructors/instructorsSlice';
 import { promotionsReducer} from '../features/promotions/promotionsSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
       courses: coursesReducer,
       comments: commentsReducer,
       instructors: instructorsReducer,
-      promotions: promotionsReducer
+      promotions: promotionsReducer,
+      user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
